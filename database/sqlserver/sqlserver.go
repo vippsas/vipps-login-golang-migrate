5fed62ba3eb3eb3f35558ced940e385d8d1d565d
Jon Kjennbakken <jon.kjennbakken@vipps.no>
2022-06-29 09:43:38 +0200

    fix

--- database/sqlserver/sqlserver.go
+++ database/sqlserver/sqlserver.go
@@ -13,10 +13,10 @@ import (
 	"go.uber.org/atomic"
 
 	"github.com/Azure/go-autorest/autorest/adal"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database"
 	"github.com/hashicorp/go-multierror"
 	mssql "github.com/microsoft/go-mssqldb" // mssql support
+	"github.com/vippsas/vipps-login-golang-migrate/v1"
+	"github.com/vippsas/vipps-login-golang-migrate/v1/database"
 )
 
 func init() {
